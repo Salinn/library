24b8c9729a4131adead7aaaff34490b6dc888a7e
Paul Darragh <pmd6624@rit.edu>
2016-08-26 02:10:30 +0000

    added validations and fixed errors

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ActiveRecord::Base
   belongs_to :library
   has_many :book_users
   has_many :users, through: :book_users
+
+  validates :title, length: { in: 2...80 }
+  validates :author, length: { in: 2...80 }
+  validates :section, length: { in: 2...80 }
+  validates :library, presence: true
 end
